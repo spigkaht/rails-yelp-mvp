783b3b7620794199138042cc61a39e10481524f2
Benjamin Jackson <benj@betterrentals.com.au>
2024-05-27 14:41:30 +1000

    completed yelp exercise

--- Gemfile
+++ Gemfile
@@ -2,6 +2,12 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+# Simple form for bootstrap forms
+gem "simple_form"
+
+# Faker for seed generation
+gem "faker"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.3"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Clearing out your junk..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+categories = %w(chinese italian japanese french belgian)
+10.times do
+  attributes = { name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: categories.sample }
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+  3.times do
+    attributes = { restaurant_id: restaurant.id, rating: (0..5).to_a.sample, content: Faker::Fantasy::Tolkien.poem }
+    review = Review.create!(attributes)
+    p restaurant
+    p review
+    review.restaurant_id = restaurant
+    puts "Created #{review.id}"
+  end
+end
+
+puts "Finished!"
